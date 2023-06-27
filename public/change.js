223a9be7f468498935099dedd745fa925d896b3a
Jabuitron <Jandresbuiitron@gmail.com>
2023-06-26 21:48:38 -0500

    estilodelnav

--- public/change.js
+++ public/change.js
@@ -1,11 +1,33 @@
+//Sobra del nav
+const $listItem = document.querySelectorAll('#navegador li')
+const $cajita = document.querySelector('#cajita')
+    $listItem.forEach((item) => {
+        item.addEventListener("mouseenter", () =>{
+            const {left,top, width, height} = item.getBoundingClientRect()
+            const {clientHeight, clientWidth} = item
+            console.log({left,top, width, height})
+            console.log({clientHeight, clientWidth})
+                $cajita.style.setProperty("--left", `${left}px`)
+                $cajita.style.setProperty("--top", `${top}px`)
+                $cajita.style.setProperty("--width", `${width}px`)
+                $cajita.style.setProperty("--height", `${height}px`)
+                $cajita.style.opacity = "1"
+                $cajita.style.visibility = "visible"                      
+            })
+            item.addEventListener("mouseleave",() =>{
+                $cajita.style.opacity = "0"
+                $cajita.style.visibility = "hidden"  
+            })
+});
+
 const observerOptions = {
     root: null,
     rootMargin: '0px',  //A partir de estos px que aparezca
     threshold: 0.50     //Porcentaje de visibilidad
 }
 
-const $container = document.querySelector(".navegador")
-let cody = document.querySelector('.section-bg2') // Lo que se verá después
+const $container = document.querySelector("#navegador")
+let cody = document.querySelector('#section-bg2') // Lo que se verá después
 
 const observer = new IntersectionObserver((entries)=>{
     console.log(entries)
